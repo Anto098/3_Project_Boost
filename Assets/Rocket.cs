380f1ca3570bad54e55a87974a168a2dcab5e7cd
Anto <leuhzebre@gmail.com>
2019-07-04 23:55:20 -0400

    level 1

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -23,6 +23,22 @@ public class Rocket : MonoBehaviour
         ProcessInput();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Safe zone");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;  
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
     private void ProcessInput()
     {
         Thrust();
